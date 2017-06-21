26ad08f0fa2a4f6bac17e7790ed23392cbd3180b
user <user@VAIO>
2017-06-21 11:01:17 +0900

    aaa

---
+++ WebContent/WEB-INF/jsp/list.jsp
@@ -0,0 +1,40 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ include file="/WEB-INF/jsp/common.jsp"%>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>申込み科目一覧</title>
+</head>
+<body>
+	<h2>申込み科目一覧</h2>
+	<a href="userlogin">ログアウト</a>
+
+	<a href="U103mousikomi">申し込み</a>
+
+	<c:forEach var="data" items="${sessionScope.entry}" varStatus="status">
+		<p>
+			<c:out value="${data.sub_name}" />
+			<a href="#"
+				onclick="document.homhom.sub_id.value='${data.sub_id}';document.homhom.num.value='1';document.homhom.submit();return false;">変更</a>
+		</p>
+		<p>
+			<c:out value="${data.sub_name2}" />
+			<a href="#"
+				onclick="document.homhom.sub_id.value='${data.sub_id2}';document.homhom.num.value='2';document.homhom.submit();return false;">変更</a>
+		</p>
+
+
+
+	</c:forEach>
+
+	<form action="SubjectChange" method="get" name="homhom">
+		<input type="hidden" name="sub_id" value="">
+		<input type="hidden" name="num" value="">
+	</form>
+
+
+</body>
+</html>
---
+++ src/servlet/SubjectChange.java
@@ -0,0 +1,55 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.SubjectDAO;
+
+/**
+ * Servlet implementation class ChangeSubject
+ */
+@WebServlet("/SubjectChange")
+public class SubjectChange extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		HttpSession session = request.getSession();
+		// ユーザーID、パスワードの取り出し
+
+		String get =  request.getParameter("sub_id");
+		String num =  request.getParameter("num");
+		System.out.println(get);
+		System.out.println(num);
+
+		SubjectDAO subject= new SubjectDAO();
+		String name = subject.subname(get);
+		System.out.println(name);
+
+		session.setAttribute("sub", name);
+		session.setAttribute("num", num);
+
+		//科目名を受け取るやつここに書く
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/userchange.jsp");
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
